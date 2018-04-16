d03406d06c9c3451f376c311a688f292ee07a317
mel <mchuam@gmail.com>
2018-04-16 18:45:59 +0000

    with getAndPrintHTMLChunks and getAndPrintHTML

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
 
   var http = require('https');
 
-function getAndPrintHTMLChunks(callback) {
+function getAndPrintHTML(callback) {
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  http.get(requestOptions, function(response) {
+    var allChunks = '';
+    response.setEncoding('utf8');
+    response.on('data', function(chunk) {
+      console.log('Chunk: '+ chunk + '\n');
+    });
+    response.on('end', function() {
+      console.log('done printing');
+    });
+  });
+
+}
+
+function getAndPrintHTMLChunks() {
   var requestOptions = {
     host: 'sytantris.github.io',
     path: '/http-examples/step1.html'
@@ -21,8 +40,10 @@ function getAndPrintHTMLChunks(callback) {
 
 }
 
+
 function printChunks(data) {
   console.log(data);
 }
 
-getAndPrintHTMLChunks(printChunks);
+// getAndPrintHTML(printChunks);
+// getAndPrintHTML();
